74278202943042224922cd28fee7ced10469b2c9
Felipe Marques <felipemarques.computacao@gmail.com>
2026-05-06 22:43:45 -0300

    refactor(trpc): move RestaurantRouter to RestaurantModule
    Remove use case providers from TrpcModule. RestaurantRouter is now provided by
    RestaurantModule which has access to all required dependencies.

--- packages/restaurant-service/src/infra/trpc/trpc.module.ts
+++ packages/restaurant-service/src/infra/trpc/trpc.module.ts
@@ -1,38 +1,24 @@
 import { Global, Module } from '@nestjs/common';
 import { TRPCModule } from 'nestjs-trpc';
-import { RestaurantRouter } from './restaurant.router';
-import { GetMenuItemUseCase } from '../../application/use-cases/get-menu-item/get-menu-item.use-case';
-import { ListMenuItemsUseCase } from '../../application/use-cases/list-menu-items/list-menu-items.use-case';
 
 /**
  * tRPC Module for Restaurant Service
  *
  * This module sets up the tRPC server using nestjs-trpc adapter.
- * Exposes RestaurantRouter procedures to other microservices.
+ * The RestaurantRouter is provided by RestaurantModule.
  *
  * Architecture:
  * - Decorator-based router with @Router(), @Query(), @Mutation()
  * - Auto-registers procedures with tRPC
  * - Full NestJS dependency injection support
- *
- * Usage:
- * - Import TrpcModule in AppModule
- * - Other services use tRPC client to call procedures
- * - Full type safety with auto-completion
  */
 @Global()
 @Module({
   imports: [
     TRPCModule.forRoot(),
   ],
-  providers: [
-    RestaurantRouter,
-    GetMenuItemUseCase,
-    ListMenuItemsUseCase,
-  ],
   exports: [
     TRPCModule,
-    RestaurantRouter,
   ],
 })
 export class TrpcModule {}
